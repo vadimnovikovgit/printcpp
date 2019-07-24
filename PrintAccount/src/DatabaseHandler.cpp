f5ca917d3475f24421a09544f1e0b4f94da3b998
Antichrist_home <vadimnovikov666@gmail.com>
2019-07-24 15:26:03 +0300

    Test last

--- PrintAccount/src/DatabaseHandler.cpp
+++ PrintAccount/src/DatabaseHandler.cpp
@@ -3,3 +3,6 @@
 
 database_handler::database_handler() :
 	m_dbServer(""), m_dbName(""), m_login(""), m_pwd(""), m_auth(false) {};
+
+database_handler::database_handler(std::string server, std::string name, std::string login, std::string pwd, bool auth) :
+	m_dbServer(server), m_dbName(name), m_login(login), m_pwd(pwd), m_auth(auth) {};
